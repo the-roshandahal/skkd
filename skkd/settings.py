f0e5d849cca5cc9a1bfc2b12a4bfc926e3107e11
the_roshandahal <roshandahal.r@gmail.com>
2025-06-28 21:30:05 +1000

    crispy

--- skkd/settings.py
+++ skkd/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'features',
-    'crispy_forms',
+    # 'crispy_forms',
 ]
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
